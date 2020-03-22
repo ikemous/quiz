dcaa72aa46ed9532644ecaa56fcf74ccf20e1cfd
Ike Barranco <barranco.ike@outlook.com>
2020-03-21 23:21:22 -0600

    Added score input to index but its not yet functional :(

--- Assets/js/myScript.js
+++ Assets/js/myScript.js
@@ -5,13 +5,14 @@
 let totalTimeLeft = 100;//Total Time for quiz in seconds
 const PENALTY = 10;
 let score = 0;//Score for The Quiz
-let questionNumber = 0//Number Of Current Question 0 = 1
+let questionNumber = 0;//Number Of Current Question 0 = 1
 let currentId = 0;//Variable Used To count through the buttons
 
 //Gather All needed elements
 let listEl = document.getElementById("buttons");
 let timerEl = document.getElementById("timer");
 let displayEl = document.getElementById("display");
+let responseEl = document.getElementById("response");
 
 
 //Define first question as an object
@@ -53,16 +54,23 @@ listEl.addEventListener("click", function(event){
 
     if(event.target.matches("button") && event.target.textContent === "START")
     {
-            populate();
-            startQuiz();
+        populate();
+        startQuiz();
+    }
+    else if(event.target.textContent === "SUBMIT")
+    {
+
     }
     else if(event.target.textContent === questionList[questionNumber].a)
     {
+        responseEl.textContent = "CORRECT!";
         questionNumber++;
-        populate();
+        populate(); 
     }
     else
     {
+        
+        responseEl.textContent = "BOOO YOU SUK!";
         let timeAfterPen = totalTimeLeft - PENALTY;
         if(timeAfterPen <= 0)
         {
@@ -78,25 +86,56 @@ listEl.addEventListener("click", function(event){
 function populate()
 {
     listEl.innerHTML = "";
-    displayEl.textContent = questionList[questionNumber].q;
     //random number to be assigned the answer
-    let randomNumber = Math.floor(Math.random() * 4);
-    let optionNumber = 0;
 
-    for(let i = 0; i < 4; i++)
+    if(questionNumber < questionList.length)
     {
-        let newButton = document.createElement("button")
-        newButton.setAttribute("data-number", i);
-        if(i === randomNumber)
+        
+        let randomNumber = Math.floor(Math.random() * 4);
+        let optionNumber = 0;
+        displayEl.textContent = questionList[questionNumber].q;
+
+        for(let i = 0; i < 4; i++)
         {
-            newButton.textContent = questionList[questionNumber].a
+            let newButton = document.createElement("button")
+            newButton.setAttribute("data-number", i);
+            if(i === randomNumber)
+            {
+                newButton.textContent = questionList[questionNumber].a
+            }
+            else
+            {
+                newButton.textContent = questionList[questionNumber].options[optionNumber];
+                optionNumber++;
+            }
+            listEl.appendChild(newButton);
         }
-        else
-        {
-            newButton.textContent = questionList[questionNumber].options[optionNumber];
-            optionNumber++;
-        }
-        listEl.appendChild(newButton);
+    }
+    else
+    {
+        displayEl.textContent = "Results!";
+        let newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "row");
+        listEl.appendChild(newDiv);
+
+        let promptText = document.createElement("h6");
+        promptText.setAttribute("class", "col-sm-3");
+        promptText.textContent = "YO INITAILS HERE";
+        newDiv.appendChild(promptText);
+
+
+        let scoreInitials = document.createElement("input");
+        scoreInitials.type = "text";
+        scoreInitials.setAttribute("class", "col-sm-6");
+        newDiv.appendChild(scoreInitials);
+
+        let submitButton = document.createElement("button");
+        submitButton.textContent = "SUBMIT"
+        submitButton.setAttribute("class", "col-sm-3");
+        newDiv.appendChild(submitButton);
+
+        totalTimeLeft = 1;
+
     }
 
 }
@@ -115,35 +154,16 @@ function startQuiz()
         //Check If Time Is Up
         if(totalTimeLeft <= 0)
         {
+            
             //Stop Doing the loop
             clearInterval(timerInteral);
+            
+            questionNumber = questionList.length;
+            populate(); 
         }
-
-
 
     }, 1000 /* Speed For The Timer = 1 Second*/);
 }//End StartQuiz()
-
-
-//while loop to force user to enter valid input
-        //Prompt the user for a pass length
-
-        //check if the input isnt a number
-            //Tell user input must be a number
-
-        //check if the input is between 8 and 128
-            //Tell user input needs to be between 8 and 128
-
-        //if input is valid
-            //break
-
-//For loop to run by the input length
-    //generate random character
-
-    //assign random character to the password
-
-
-
 
 
 
